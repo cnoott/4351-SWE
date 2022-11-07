2a5e84e38f30395dfaebf8a9766636017afdc534
Liam Amadio <liamamadio@gmail.com>
2022-11-07 17:00:30 -0600

    first push did user login stuff

---
+++ app.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const cors = require('cors');
+
+require('dotenv').config();
+
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors({
+    origin: '*',
+}));
+
+//import routes here
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+
+//routes
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+
+const PORT = process.env.PORT || 8000;
+
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('database connected');
+    });
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ controllers/auth.js
@@ -0,0 +1,80 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const { v1: uuidv1 } = require('uuid');
+
+require('dotenv').config();
+
+exports.signup = (req, res) => {
+    console.log(req.body);
+
+    const user = new User(req.body);
+
+    user.save((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: '' + err
+            });
+        }
+
+        user.salt = undefined;
+        user.hashed_password = undefined;
+
+        const token = jwt.sign({ _id: user._id}, process.env.JWT_SECRET);
+
+        res.cookie('t', token, { expiry: new Date() + 999 });
+
+        const { _id, username } = user;
+        return res.json({ token, user: { _id, username } });
+    });
+};
+
+exports.signin = (req, res) => {
+    //find user based on email
+    const { username, password } = req.body;
+    User.findOne({ username }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist'
+            });
+        }
+
+        //if user is found make sure email and password match
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password don't match"
+            });
+        }
+
+        //generate signin token with user id and secret
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        //persist token as 't' in cookie with expiry date
+        res.cookie('t', token, { expiry: new Date() + 150 });
+
+        //return response with user and token to frontend client
+        const { _id, username } = user;
+        return res.json({ token, user: { _id, username } });
+    });
+};
+
+exports.signout = (req, res, next) => {
+    res.clearCookie('t');
+    res.json({ message: 'Signout Success' });
+};
+
+//requireSignin
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256'],
+    userProperty: 'auth'
+});
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if(!user) {
+        return res.status(403).json({
+            error: 'Access denied'
+        });
+    }
+    next();
+};
