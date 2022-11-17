c50ea8e63fb438236858767f7d548a0de10886a9
Tontallini <57695988+Tontallini@users.noreply.github.com>
2022-11-17 15:39:01 -0600

    Guest Reservation Page

--- swe-frontend/src/RootElement.js
+++ swe-frontend/src/RootElement.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './App';
 import Login from './user/Login';
 import Signup from './user/Signup';
+import GuestReserve from './user/GuestReserve';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ const RootElement = () => {
         <Routes>
             <Route path='/' exact element={<Login/>}/>
             <Route path='/signup' exact element={<Signup/>}/>
+            <Route path='/guestReserve' exact element={<GuestReserve/>}/>
         </Routes>
     </BrowserRouter>
     );
